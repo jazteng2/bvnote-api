4378810054920632adb2000a6cd3c15b668ee606
Jared Teng <jaredteng1602@gmail.com>
2023-11-16 02:47:29 +1100

    major change: separate data access logic code to services layer.

--- bvnote-web-api/RouteGroup/BibleGroup.cs
+++ bvnote-web-api/RouteGroup/BibleGroup.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using bvnote_web_api.Data.DTO;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using bvnote_web_api.Services;
 
 namespace bvnote_web_api.RouteGroup
 {
@@ -9,27 +11,27 @@ namespace bvnote_web_api.RouteGroup
     {
         public static RouteGroupBuilder MapBible(this RouteGroupBuilder group)
         {
-            group.MapGet("/books", async (BvnV1Context db) =>
-            {
-                var dbBooks = await db.Books.ToListAsync();
 
-                return dbBooks.Count() > 0 && dbBooks is not null
-                    ? Results.Ok(BookDTO.GetBookDTOs(dbBooks))
+            group.MapGet("/books", async (BookService bookService) =>
+            {
+                var books = await bookService.GetBooksAsync();
+
+                return books is not null
+                    ? Results.Ok(books)
                     : Results.NotFound("Unable to fetch books");
             });
 
-            group.MapGet("/books/{bookId}", async (string bookId, BvnV1Context db) =>
+
+            group.MapGet("/books/{bookId}", async (string bookId, BookService bookService) =>
             {
-                var book = await db.Books
-                    .Where(b => b.BookId == bookId)
-                    .FirstOrDefaultAsync();
+                var book = bookService.GetBookAsync(bookId);
 
                 return book is not null
-                    ? Results.Ok(BookDTO.GetBookDTO(book))
+                    ? Results.Ok(book)
                     : Results.NotFound("Unable to fetch book with Id " + bookId);
             });
 
-            group.MapGet("/books/{bookId}/verses", async (string bookId, [FromQuery] int chapterNo, BvnV1Context db) =>
+            /*group.MapGet("/books/{bookId}/verses", async (string bookId, [FromQuery] int chapterNo, BvnV1Context db) =>
             {
                 var book = await db.Books.FindAsync(bookId);
 
@@ -55,7 +57,7 @@ namespace bvnote_web_api.RouteGroup
                     ? Results.Ok(AbbrevDTO.GetAbbrevDTOs(abbrevs))
                     : Results.NotFound("Unable to fetch abbreviations");
 
-            });
+            });*/
 
             return group;
         }
