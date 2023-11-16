196cf9043034c46f59d526dd5adb92d2fb686c29
Jared Teng <jaredteng1602@gmail.com>
2023-11-17 00:54:15 +1100

    reformat syntax reduce code

--- bvnote-web-api/RouteGroup/BibleGroup.cs
+++ bvnote-web-api/RouteGroup/BibleGroup.cs
@@ -1,6 +1,4 @@
-﻿using bvnote_web_api.Data;
-using Microsoft.AspNetCore.Mvc;
-using bvnote_web_api.Data.DTO;
+﻿using Microsoft.AspNetCore.Mvc;
 using bvnote_web_api.Services;
 
 namespace bvnote_web_api.RouteGroup
@@ -9,29 +7,17 @@ namespace bvnote_web_api.RouteGroup
     {
         public static RouteGroupBuilder MapBible(this RouteGroupBuilder group)
         {
-            group.MapGet("/books", async (BookService bookService) =>
-            {
-                var books = await bookService.GetBooksAsync();
-                return Results.Ok(books);
-            });
+            group.MapGet("/books", async (BookService bookService)
+                => Results.Ok(await bookService.GetBooksAsync()));
 
-            group.MapGet("/books/{bookId:length(10)}", async (string bookId, BookService bookService) =>
-            {
-                var book = await bookService.GetBookAsync(bookId);
-                return Results.Ok(book);
-            });
+            group.MapGet("/books/{bookId:length(10)}", async (string bookId, BookService bookService)
+                => Results.Ok(await bookService.GetBookAsync(bookId)));
 
-            group.MapGet("/books/{bookId:length(10)}/verses", async (string bookId, [FromQuery] int chapterNo, BookService bookService) =>
-            {
-                var verses = await bookService.GetChapterVerses(bookId, chapterNo);
-                return Results.Ok(verses);
-            });
+            group.MapGet("/books/{bookId:length(10)}/verses", async (string bookId, [FromQuery] int chapterNo, BookService bookService)
+                => Results.Ok(await bookService.GetChapterVerses(bookId, chapterNo)));
 
-            group.MapGet("/books/verses", async ([FromQuery] string abbrev, [FromQuery] int chapterNo, BookService bookService) =>
-            {
-                var verses = await bookService.GetChapterVerses_abbrev(abbrev, chapterNo);
-                return Results.Ok(verses);
-            });
+            group.MapGet("/books/verses", async ([FromQuery] string abbrev, [FromQuery] int chapterNo, BookService bookService)
+                => Results.Ok(await bookService.GetChapterVerses_abbrev(abbrev, chapterNo)));
 
             return group;
         }
