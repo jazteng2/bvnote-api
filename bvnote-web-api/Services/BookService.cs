9634c3c3773ace1911340854948c4fec62067594
Jared Teng <jaredteng1602@gmail.com>
2023-11-17 00:47:00 +1100

    new methods for get verse

--- bvnote-web-api/Services/BookService.cs
+++ bvnote-web-api/Services/BookService.cs
@@ -18,59 +18,83 @@ namespace bvnote_web_api.Services
 
         public async Task<List<BookDTO>> GetBooksAsync()
         {
-            // check cache value
+            // get cache value
             var cacheKey = "books";
-            if (_memoryCache.TryGetValue(cacheKey, out List<BookDTO> cacheValue))
-            {
+            if (_memoryCache.TryGetValue(cacheKey, out List<BookDTO>? cacheValue))
                 return cacheValue is null
                     ? new List<BookDTO>()
                     : cacheValue;
-            }
 
-            // check books
+            // get fresh data from db
             var books = await _db.Books.ToListAsync();
-            if (books is null) 
-            {
-                return new List<BookDTO>();
-            }
+            if (books is null) return new List<BookDTO>();
 
+            // set new cache memory
             var cacheEntryOptions = new MemoryCacheEntryOptions()
                             .SetSlidingExpiration(TimeSpan.FromSeconds(30))
                             .SetAbsoluteExpiration(TimeSpan.FromMinutes(1))
-                            .SetPriority(CacheItemPriority.Normal);
+                            .SetPriority(CacheItemPriority.Low);
 
             _memoryCache.Set(cacheKey, books, cacheEntryOptions);
-
             return BookDTO.GetBookDTOs(books);
         }
 
         public async Task<BookDTO> GetBookAsync(string bookId)
         {
-            // check cache value
-            var cacheKey = "book";
-            if (_memoryCache.TryGetValue(cacheKey, out BookDTO cacheValue))
-            {
+            // get cache value
+            var cacheKey = "book_" + bookId;
+            if (_memoryCache.TryGetValue(cacheKey, out BookDTO? cacheValue))
                 return cacheValue is null
                     ? new BookDTO()
                     : cacheValue;
-            }
 
-            var book = await _db.Books
-                    .Where(b => b.BookId == bookId)
-                    .FirstOrDefaultAsync();
-            if (book is null)
-            {
-                return new BookDTO();
-            }
+            // get fresh data from db
+            var book = await _db.Books.Where(b => b.BookId == bookId).FirstOrDefaultAsync();
+            if (book is null) return new BookDTO();
 
+            // set new cache memory
             var cacheEntryOptions = new MemoryCacheEntryOptions()
-                            .SetSlidingExpiration(TimeSpan.FromSeconds(30))
-                            .SetAbsoluteExpiration(TimeSpan.FromMinutes(1))
+                            .SetSlidingExpiration(TimeSpan.FromSeconds(60))
+                            .SetAbsoluteExpiration(TimeSpan.FromMinutes(3))
                             .SetPriority(CacheItemPriority.Normal);
 
             _memoryCache.Set(cacheKey, book, cacheEntryOptions);
-
             return BookDTO.GetBookDTO(book);
+        }
+
+        public async Task<List<VerseDTO>> GetChapterVerses(string bookId, int chapterNo)
+        {
+            // get cache value
+            var cacheKey = "verses_" + bookId + "_" + chapterNo;
+            if (_memoryCache.TryGetValue(cacheKey,out List<VerseDTO>? cacheValue)) 
+                return cacheValue is null
+                    ? new List<VerseDTO>() 
+                    : cacheValue;
+
+            // get fresh data from db
+            var verses = await _db.Verses
+                    .Where(v => v.ChapterNo == chapterNo && v.BookId == bookId)
+                    .OrderBy(v => v.VerseNo)
+                    .ToListAsync();
+            if (verses is null) return new List<VerseDTO>();
+
+            // set new cache memory
+            var cacheEntryOptions = new MemoryCacheEntryOptions()
+                            .SetSlidingExpiration(TimeSpan.FromMinutes(5))
+                            .SetAbsoluteExpiration(TimeSpan.FromMinutes(10))
+                            .SetPriority(CacheItemPriority.High);
+            _memoryCache.Set(cacheKey, verses, cacheEntryOptions);
+            return VerseDTO.GetVerseDTOs(verses);
+        }
+
+        public async Task<List<VerseDTO>> GetChapterVerses_abbrev(string bookAbbrev, int chapterNo)
+        {
+            var abbrev = await _db.Abbrevs
+                .Where(a => a.Abbreviation.ToLower().Equals(bookAbbrev.ToLower()))
+                .FirstOrDefaultAsync();
+            return abbrev is null 
+                ? new List<VerseDTO>() 
+                : await GetChapterVerses(abbrev.BookId, chapterNo);
         }
     }
 }
