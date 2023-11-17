e81d210ee469ce9a8acd454c3d39fc6440548d74
jazteng2 <jaredteng1602@gmail.com>
2023-11-17 12:37:00 +1100

    Renaming and added document endpoints to setup for editor feature

--- bvnote-web-api/RouteGroup/BvnGroup.cs
+++ bvnote-web-api/RouteGroup/BvnGroup.cs
@@ -3,17 +3,17 @@ using bvnote_web_api.Services;
 
 namespace bvnote_web_api.RouteGroup
 {
-    public static class BibleGroup
+    public static class BvnGroup
     {
         public static RouteGroupBuilder MapBible(this RouteGroupBuilder group)
         {
             group.MapGet("/books", async (IBookService bookService)
                 => Results.Ok(await bookService.GetBooksAsync()));
 
-            group.MapGet("/books/{bookId:length(10)}", async (string bookId, IBookService bookService)
+            group.MapGet("/books/{bookId}", async (string bookId, IBookService bookService)
                 => Results.Ok(await bookService.GetBookAsync(bookId)));
 
-            group.MapGet("/books/{bookId:length(10)}/verses", async (string bookId, [FromQuery] int chapterNo, IBookService bookService)
+            group.MapGet("/books/{bookId}/verses", async (string bookId, [FromQuery] int chapterNo, IBookService bookService)
                 => Results.Ok(await bookService.GetChapterVerses(bookId, chapterNo)));
 
             group.MapGet("/books/verses", async ([FromQuery] string abbrev, [FromQuery] int chapterNo, IBookService bookService)
@@ -28,6 +28,40 @@ namespace bvnote_web_api.RouteGroup
             {
 
             });
+
+            // Search
+            group.MapGet("/documents", ([FromQuery] string find) =>
+            {
+
+            });
+
+            group.MapGet("/documents/{id}", (string id, [FromQuery] string find) =>
+            {
+
+            });
+
+            // By Verses
+            group.MapGet("/verses/{id}/documents", () =>
+            {
+
+            });
+
+            group.MapGet("/verses/{id}/documents/{id}", () =>
+            {
+
+            });
+
+            // By Videos
+            group.MapGet("/videos/{id}/documents", () =>
+            {
+
+            });
+
+            group.MapGet("/videos/{id}/documents/{id}", () =>
+            {
+
+            });
+
             return group;
         }
     }
