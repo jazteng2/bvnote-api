5e3d89d6ea7fef8b8f2a02734c517a868804a447
Jared Teng <jaredteng1602@gmail.com>
2023-11-24 02:59:34 +1100

    include error message

--- bvnote-web-api/RouteGroup/BvnGroup.cs
+++ bvnote-web-api/RouteGroup/BvnGroup.cs
@@ -7,23 +7,36 @@ namespace bvnote_web_api.RouteGroup
     {
         public static RouteGroupBuilder MapBible(this RouteGroupBuilder group)
         {
-            group.MapGet("/books", async (IBookService bookService)
-                => Results.Ok(await bookService.GetBooksAsync()));
+            group.MapGet("/books", async (IBookService bookService) =>
+            {
+                var books = await bookService.GetBooksAsync();
+                return books is null ? Results.NotFound("Books does not exist") : Results.Ok(books);
+            });
 
-            group.MapGet("/books/{bookId}", async (string bookId, IBookService bookService)
-                => Results.Ok(await bookService.GetBookAsync(bookId)));
+            group.MapGet("/books/{bookId}", async (string bookId, IBookService bookService) =>
+            {
+                var book = await bookService.GetBookAsync(bookId);
+                return book is null ? Results.NotFound("Book does not exist") : Results.Ok(book);
+            });
 
-            group.MapGet("/books/{bookId}/verses", async (string bookId, [FromQuery] int chapterNo, IBookService bookService)
-                => Results.Ok(await bookService.GetChapterVerses(bookId, chapterNo)));
+            group.MapGet("/books/{bookId}/verses", async (string bookId, [FromQuery] int chapterNo, IBookService bookService) =>
+            {
+                var verses = await bookService.GetChapterVerses(bookId, chapterNo);
+                return verses is null ? Results.NotFound("Verses does not exist") : Results.Ok(verses);
+            });
 
-            group.MapGet("/books/verses", async ([FromQuery] string abbrev, [FromQuery] int chapterNo, IBookService bookService)
-                => Results.Ok(await bookService.GetChapterVerses_abbrev(abbrev, chapterNo)));
+            group.MapGet("/books/verses", async ([FromQuery] string abbrev, [FromQuery] int chapterNo, IBookService bookService) =>
+            {
+                var verses = await bookService.GetChapterVerses_abbrev(abbrev, chapterNo);
+                return verses is null ? Results.NotFound("Verses does not exist") : Results.Ok(verses);
+            });
 
             return group;
         }
 
         public static RouteGroupBuilder MapDocuments(this RouteGroupBuilder group)
         {
+
             group.MapGet("/documents/{id}", () =>
             {
 
